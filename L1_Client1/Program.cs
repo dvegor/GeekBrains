99ebe21dfc7c66788df7513c64af6f98398a0e14
d_ego <dvegor@gmail.com>
2024-02-15 14:45:06 +0400

    init commit

--- L1_Client1/Program.cs
+++ L1_Client1/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace L1_Client1
+﻿using System.Net.Sockets;
+using System.Security;
+
+namespace L1_Client1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            TcpClient tcpClient = new TcpClient();
+            Thread.Sleep(1000);
+            tcpClient.Connect("127.0.0.1", 55555);
+
+            Console.ReadLine();
         }
     }
 }
--- L1_Server/Program.cs
+++ L1_Server/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace L1_Server
+﻿    using System.Net;
+    using System.Net.Sockets;
+
+namespace L1_Server
 {
-    internal class Program
-    {
-        static void Main(string[] args)
+        internal class Program
         {
-            Console.WriteLine("Hello, World!");
+            static async Task Main(string[] args)
+            {
+                var server = new ChatServer();
+                await server.Run();
+            }
+        }
+        public class ChatServer
+        {
+            TcpListener listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 55555);
+            public async Task Run()
+            {
+                try
+                {
+                    listener.Start();
+                    await Console.Out.WriteLineAsync("Запущен");
+
+                    while (true)
+                    {
+                        var tcpClient = await listener.AcceptTcpClientAsync();
+                        Console.WriteLine("Успешно подключен");
+
+                        Task.Run(() => ProcessClient(tcpClient));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    await Console.Out.WriteLineAsync($"Error: {ex.Message}");
+                }
+
+            }
+            public async Task ProcessClient(TcpClient client)
+            {
+                var reader = new StreamReader(client.GetStream());
+                var messege = await reader.ReadLineAsync();
+                Console.WriteLine("asd");
+            }
         }
     }
-}
