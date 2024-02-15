e9ea57e5264459d0369fdbc94246a389ee5d1248
d_ego <dvegor@gmail.com>
2024-02-15 14:42:07 +0400

    Добавьте файлы проекта.

---
+++ L1_Client1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace L1_Client1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ L1_Server/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace L1_Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
